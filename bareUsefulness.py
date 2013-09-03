19bf2b128a8ad896dd1954af03a32bf51dba2017
pschrader <phil.schrader@gmail.com>
2013-09-02 22:33:22 -0500

    added showRelated, keywordSearch, and added formatting to output

--- bareUsefulness.py
+++ bareUsefulness.py
@@ -1,24 +1,67 @@
 from crud import *
 from py2neo import cypher
 
-def showThoughts():
-    query = 'START n=node(*) RETURN n, n.name!'
+def showThoughts(snippitLen=30):
+    snippitLine = "="
+    for i in range(1,snippitLen):
+        snippitLine += "="
+    query = 'START n=node(*) \
+        WHERE HAS (n.text) \
+        RETURN n'
+    data, metadata = cypher.execute(graph_db, query)
+    print 'ID' + '\t' + 'Thought'
+    print '====' + '\t' + snippitLine
+    for row in data:
+        print row[0]._id, '\t', textSnippit(row[0]._id, snippitLen)
+
+def keywordSearch(keyword):
+    query = 'START n=node(*) \
+        WHERE n.text! =~ "(?i).*' + keyword + '.*" \
+        RETURN n'
     data, metadata = cypher.execute(graph_db, query)
     for row in data:
-        print row[0]['text']
+        print row[0]._id, '\t', row[0]['text']
+    
+def showRelated(nodeID,snippitLen=30):
+    print textSnippit(nodeID)
+    print '========'
+    #first print outgoing relationships
+    queryOutgoing = 'START n=node(' + str(nodeID) + ') \
+            MATCH (n)-[r]->(m) \
+            RETURN m, type(r)'
+    data, metadata = cypher.execute(graph_db, queryOutgoing)
+    for row in data:
+        print row[1], '->', row[0]['text'], '(' + str(row[0]._id) +')'
+    print ''
+    #next print incoming relationships
+    queryOutgoing = 'START n=node(' + str(nodeID) + ') \
+            MATCH (n)<-[r]-(m) \
+            RETURN m, type(r)'
+    data, metadata = cypher.execute(graph_db, queryOutgoing)
+    for row in data:
+        print '<-', row[1], row[0]['text'], '(' + str(row[0]._id) +')'
+    
 
-def popularity():
+def popularity(snippitLen=30):
     """this function displays snippits of all the thoughts in your
     notebook in descending order based on the number of thoughts each
     thought is connected to"""
+    snippitSpaces = " "
+    for i in range(1,snippitLen):
+        snippitSpaces += " "
+    snippitLine = "="
+    for i in range(1,snippitLen):
+        snippitLine += "="
     query = 'START n=node(*) \
         MATCH (n)--(m) \
         WHERE HAS (n.text) \
         RETURN n, count(DISTINCT m) \
         ORDER BY count(DISTINCT m) DESC'
     data, metadata = cypher.execute(graph_db, query)
+    print 'ID' + '\t' + 'Thought' + snippitSpaces[:-7] + '\t' + 'Count Linked'
+    print '====' '\t' + snippitLine + '\t' + '============'
     for row in data:
-        print row[0]._id, '\t', textSnippit(row[0]._id), '\t', row[1]
+        print row[0]._id, '\t', textSnippit(row[0]._id, snippitLen), '\t', row[1]
 
 def textSnippit(nodeID, snippitLen = 32):
     """this function is for use when displaying lists of thoughts
